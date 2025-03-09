59749f8f02d3e7a57cce1508c1259d2e8df68392
Md Mustafizur Rahman <moukhkst@gmail.com>
2025-03-09 15:45:08 -0700

    added prep data for also injection for repo

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
