3eb2d28a0c0646fa7187bbf1dc5f3c93eec86a92
Md Mustafizur Rahman <moukhkst@gmail.com>
2025-03-11 18:31:56 -0700

    Services internal sync call test

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
 // using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService.Controllers
@@ -17,19 +18,19 @@ namespace PlatformService.Controllers
     {
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
-        //private readonly ICommandDataClient _commandDataClient;
+        private readonly ICommandDataClient _commandDataClient;
         //private readonly IMessageBusClient _messageBusClient;
 
         public PlatformsController(
             IPlatformRepo repository, 
-            IMapper mapper
-            //ICommandDataClient commandDataClient,
+            IMapper mapper,
+            ICommandDataClient commandDataClient
             //IMessageBusClient messageBusClient
             )
         {
             _repository = repository;
             _mapper = mapper;
-            //_commandDataClient = commandDataClient;
+            _commandDataClient = commandDataClient;
             //_messageBusClient = messageBusClient;
         }
 
@@ -56,7 +57,7 @@ namespace PlatformService.Controllers
         }
 
         [HttpPost]
-        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        public async Task<ActionResult<PlatformReadDto>> CreatePlatform(PlatformCreateDto platformCreateDto)
         {
             var platformModel = _mapper.Map<Platform>(platformCreateDto);
 
@@ -65,6 +66,16 @@ namespace PlatformService.Controllers
             _repository.SaveChanges();
 
             var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            // Send Sync Message
+            try
+            {
+                await _commandDataClient.SendPlatformToCommand(platformReadDto);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
+            }
 
             return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformReadDto.Id}, platformReadDto);
 
--- PlatformService/obj/Debug/net9.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net9.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea35e7299e669bb4f3842f17576028b271cca04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66fdfdb55ae254f67c1ace16b7fec0f1c41ac4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
