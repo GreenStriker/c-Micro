b66fdfdb55ae254f67c1ace16b7fec0f1c41ac4d
Md Mustafizur Rahman <moukhkst@gmail.com>
2025-03-11 18:03:16 -0700

    2nd service added

---
+++ CommandsService/Controller/PlatformsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("---> Inbound Post call # Command Service");
+
+            return Ok("Inbound Test of from platform Controller");
+        }
+    }
+}
--- PlatformService/obj/Debug/net9.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net9.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d834b50c3fb1b7f2135a04b22db04211cdd2fb63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea35e7299e669bb4f3842f17576028b271cca04")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
